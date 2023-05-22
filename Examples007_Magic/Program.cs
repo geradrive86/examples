30c60deae79b634f2d69a05f6e540019190f1ec5
geradrive86 <geradrive86@gmail.com>
2023-05-22 16:18:26 +0300

    Получился треугольник

--- Examples007_Magic/Program.cs
+++ Examples007_Magic/Program.cs
@@ -2,12 +2,12 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 16;
+int xa = 40;
 int ya = 1;
 int xb = 1;
-int yb = 32;
-int xc = 32;
-int yc = 32;
+int yb = 30;
+int xc = 80;
+int yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
